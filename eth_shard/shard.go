5554060b634f969a6c865b8bc5ad1e6bbe53a0ec
xfluo2014 <xfluo2014@gmail.com>
2024-07-24 22:03:33 +0800

    修改PBFT前错误，正在修改注入交易问题

--- eth_shard/shard.go
+++ eth_shard/shard.go
@@ -95,6 +95,7 @@ func ExecutionInitialize(shardID uint32, _node *eth_node.EthNode, height2Reconfi
 }
 
 func (s *Shard) Start() {
+	log.Debug("Execution Start")
 	s.ExecutionStart()
 }
 
@@ -132,6 +133,7 @@ func (s *Shard) addGenesisTB() {
 		Target_nodeAddr: cfg.BooterAddr,
 	}
 
+	log.Debug("start to send genesis")
 	s.messageHub.Send(core.MsgTypeShardSendGenesis, 0, genesis, nil)
 }
 
